3917875ca0946a3347f6fdee046ab68418d502d1
Weiming Zhai <wmzhai@gmail.com>
2015-12-27 15:23:43 +0800

    configuration

--- useraccounts-configuration.js
+++ useraccounts-configuration.js
@@ -1,5 +1,6 @@
 AccountsTemplates.configure({
     showForgotPasswordLink: true,
+    forbidClientAccountCreation: true,
     texts: {
         errors: {
             loginForbidden: "用户或密码错误",
@@ -8,6 +9,19 @@ AccountsTemplates.configure({
         title: {
             signIn: "登录",
             signUp: "注册",
+        },
+        button: {
+            changePwd: "密码",
+            enrollAccount: "Enroll Text",
+            forgotPwd: "忘记密码",
+            resetPwd: "重置密码",
+            signIn: "登录",
+            signUp: "注册",
+        },
+        inputIcons: {
+            isValidating: "fa fa-spinner fa-spin",
+            hasSuccess: "fa fa-check",
+            hasError: "fa fa-times",
         }
-    },
+    }
 });
