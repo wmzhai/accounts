672b652640cc739cd40386ca9e1f957a15bd3ca0
Weiming Zhai <wmzhai@gmail.com>
2015-12-27 14:24:57 +0800

    configuration

---
+++ useraccounts-configuration.js
@@ -0,0 +1,13 @@
+AccountsTemplates.configure({
+    showForgotPasswordLink: true,
+    texts: {
+        errors: {
+            loginForbidden: "用户或密码错误",
+            pwdMismatch: "密码不匹配",
+        },
+        title: {
+            signIn: "登录",
+            signUp: "注册",
+        }
+    },
+});
